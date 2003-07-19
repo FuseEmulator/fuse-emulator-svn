5259760630b819bb52c58c39a4e9fdcdbcc5cdc3
(no author) <(no author)@82cbdd72-22ac-47d8-a7df-4a7c34dd7743>
2003-07-19 09:17:13 +0000

    This commit was manufactured by cvs2svn to create branch 'libspectrum_0_1_0pre1-crypto-branch'.
    git-svn-id: svn://svn.code.sf.net/p/fuse-emulator/code/branches/libspectrum_0_1_0pre1-crypto-branch@1493 82cbdd72-22ac-47d8-a7df-4a7c34dd7743

---
+++ libspectrum/snapshot.c
@@ -0,0 +1,212 @@
+/* snapshot.c: Snapshot handling routines
+   Copyright (c) 2001-2003 Philip Kendall, Darren Salt
+
+   $Id$
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+   Author contact information:
+
+   E-mail: pak21-fuse@srcf.ucam.org
+   Postal address: 15 Crescent Road, Wokingham, Berks, RG40 2DB, England
+
+*/
+
+#include <config.h>
+
+#include <string.h>
+
+#include "internals.h"
+
+/* Some flags which may be given to libspectrum_snap_write() */
+const int LIBSPECTRUM_FLAG_SNAPSHOT_NO_COMPRESSION = 1 << 0;
+
+/* Some flags which may be returned from libspectrum_snap_write() */
+const int LIBSPECTRUM_FLAG_SNAPSHOT_MINOR_INFO_LOSS = 1 << 0;
+const int LIBSPECTRUM_FLAG_SNAPSHOT_MAJOR_INFO_LOSS = 1 << 1;
+
+/* Free all memory used by a libspectrum_snap structure (destructor...) */
+libspectrum_error
+libspectrum_snap_free( libspectrum_snap *snap )
+{
+  int i;
+
+  for( i = 0; i < 8; i++ )
+    if( libspectrum_snap_pages( snap, i ) )
+	free( libspectrum_snap_pages( snap, i ) );
+
+  for( i = 0; i < 256; i++ ) {
+    if( libspectrum_snap_slt_length( snap, i ) ) {
+      free( libspectrum_snap_slt( snap, i ) );
+      libspectrum_snap_set_slt_length( snap, i, 0 );
+    }
+  }
+  if( libspectrum_snap_slt_screen( snap ) )
+  free( libspectrum_snap_slt_screen( snap ) );
+
+  free( snap );
+
+  return LIBSPECTRUM_ERROR_NONE;
+}
+
+/* Read in a snapshot, optionally guessing what type it is */
+libspectrum_error
+libspectrum_snap_read( libspectrum_snap *snap, const libspectrum_byte *buffer,
+		       size_t length, libspectrum_id_t type,
+		       const char *filename )
+{
+  libspectrum_class_t class;
+  libspectrum_error error;
+
+  /* If we don't know what sort of file this is, make a best guess */
+  if( type == LIBSPECTRUM_ID_UNKNOWN ) {
+    error = libspectrum_identify_file( &type, filename, buffer, length );
+    if( error ) return error;
+
+    /* If we still can't identify it, give up */
+    if( type == LIBSPECTRUM_ID_UNKNOWN ) {
+      libspectrum_print_error(
+        LIBSPECTRUM_ERROR_UNKNOWN,
+	"libspectrum_snap_read: couldn't identify file"
+      );
+      return LIBSPECTRUM_ERROR_UNKNOWN;
+    }
+  }
+
+  error = libspectrum_identify_class( &class, type );
+  if( error ) return error;
+
+  if( class != LIBSPECTRUM_CLASS_SNAPSHOT ) {
+    libspectrum_print_error( LIBSPECTRUM_ERROR_CORRUPT,
+			     "libspectrum_snap_read: not a snapshot file" );
+    return LIBSPECTRUM_ERROR_CORRUPT;
+  }
+
+  switch( type ) {
+
+  case LIBSPECTRUM_ID_SNAPSHOT_PLUSD:
+    return libspectrum_plusd_read( snap, buffer, length );
+
+  case LIBSPECTRUM_ID_SNAPSHOT_SNA:
+    return libspectrum_sna_read( snap, buffer, length );
+
+  case LIBSPECTRUM_ID_SNAPSHOT_SNP:
+    return libspectrum_snp_read( snap, buffer, length );
+
+  case LIBSPECTRUM_ID_SNAPSHOT_SP:
+    return libspectrum_sp_read( snap, buffer, length );
+
+  case LIBSPECTRUM_ID_SNAPSHOT_SZX:
+    return libspectrum_szx_read( snap, buffer, length );
+
+  case LIBSPECTRUM_ID_SNAPSHOT_Z80:
+    return libspectrum_z80_read( snap, buffer, length );
+
+  case LIBSPECTRUM_ID_SNAPSHOT_ZXS:
+    return libspectrum_zxs_read( snap, buffer, length );
+
+  /* Should never reach here as all non-snapshot types removed above */
+  default:
+    libspectrum_print_error( LIBSPECTRUM_ERROR_LOGIC,
+			     "libspectrum_snap_read: unknown snapshot type %d",
+			     type );
+    return LIBSPECTRUM_ERROR_LOGIC;
+  }
+
+}
+
+libspectrum_error
+libspectrum_snap_write( libspectrum_byte **buffer, size_t *length,
+			int *out_flags, libspectrum_snap *snap,
+	 		libspectrum_id_t type, libspectrum_creator *creator,
+			int in_flags )
+{
+  libspectrum_class_t class;
+  libspectrum_error error;
+
+  error = libspectrum_identify_class( &class, type );
+  if( error ) return error;
+
+  if( class != LIBSPECTRUM_CLASS_SNAPSHOT ) {
+    libspectrum_print_error( LIBSPECTRUM_ERROR_INVALID,
+			     "libspectrum_snap_write: not a snapshot format" );
+    return LIBSPECTRUM_ERROR_INVALID;
+  }
+
+  switch( type ) {
+
+  case LIBSPECTRUM_ID_SNAPSHOT_SNA:
+    return libspectrum_sna_write( buffer, length, out_flags, snap, in_flags );
+
+  case LIBSPECTRUM_ID_SNAPSHOT_SZX:
+    return libspectrum_szx_write( buffer, length, out_flags, snap, creator,
+				  in_flags );
+
+  case LIBSPECTRUM_ID_SNAPSHOT_Z80:
+    return libspectrum_z80_write2( buffer, length, out_flags, snap, in_flags );
+
+  default:
+    libspectrum_print_error( LIBSPECTRUM_ERROR_UNKNOWN,
+			     "libspectrum_snap_write: format not supported" );
+    return LIBSPECTRUM_ERROR_UNKNOWN;
+
+  }
+}
+
+/* Given a 48K memory dump `data', place it into the
+   appropriate bits of `snap' for a 48K machine */
+int
+libspectrum_split_to_48k_pages( libspectrum_snap *snap,
+				const libspectrum_byte* data )
+{
+  libspectrum_byte *buffer[3];
+  size_t i, j;
+
+  /* If any of the three pages are already occupied, barf */
+  if( libspectrum_snap_pages( snap, 5 ) ||
+      libspectrum_snap_pages( snap, 2 ) ||
+      libspectrum_snap_pages( snap, 0 )    ) {
+    libspectrum_print_error(
+      LIBSPECTRUM_ERROR_LOGIC,
+      "libspectrum_split_to_48k_pages: RAM page already in use"
+    );
+    return LIBSPECTRUM_ERROR_LOGIC;
+  }
+
+  for( i = 0; i < 3; i++ ) {
+    buffer[i] = malloc( 0x4000 * sizeof( libspectrum_byte ) );
+
+    if( !buffer[i] ) {
+      for( j = 0; j < i; j++ ) free( buffer[i] );
+      libspectrum_print_error(
+        LIBSPECTRUM_ERROR_MEMORY,
+	"libspectrum_split_to_48k_pages: out of memory"
+      );
+      return LIBSPECTRUM_ERROR_MEMORY;
+    }
+
+  }
+
+  libspectrum_snap_set_pages( snap, 5, buffer[0] );
+  libspectrum_snap_set_pages( snap, 2, buffer[1] );
+  libspectrum_snap_set_pages( snap, 0, buffer[2] );
+
+  /* Finally, do the copies... */
+  memcpy( libspectrum_snap_pages( snap, 5 ), &data[0x0000], 0x4000 );
+  memcpy( libspectrum_snap_pages( snap, 2 ), &data[0x4000], 0x4000 );
+  memcpy( libspectrum_snap_pages( snap, 0 ), &data[0x8000], 0x4000 );
+
+  return LIBSPECTRUM_ERROR_NONE;
+}
---
+++ libspectrum/timings.c
@@ -0,0 +1,164 @@
+/* timings.c: Timing routines
+   Copyright (c) 2003 Philip Kendall
+
+   $Id$
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+   Author contact information:
+
+   E-mail: pak21-fuse@srcf.ucam.org
+   Postal address: 15 Crescent Road, Wokingham, Berks, RG40 2DB, England
+
+*/
+
+#include <config.h>
+
+#include <string.h>
+
+#include "internals.h"
+
+/* The frame timings of a machine */
+typedef struct timings_t {
+
+  /* Processor speed in Hz */
+  libspectrum_dword processor_speed;
+
+  /* AY clock speed in Hz */
+  libspectrum_dword ay_speed;
+
+  /* Line timings in tstates */
+  libspectrum_word left_border, horizontal_screen, right_border,
+	           horizontal_retrace;
+
+  /* Frame timings in lines */
+  libspectrum_word top_border, vertical_screen, bottom_border,
+	           vertical_retrace;
+
+  /* How long after interrupt is the top-level pixel of the main screen
+     displayed */
+  libspectrum_dword top_left_pixel;
+
+} timings_t;
+
+/* The actual data from which the full timings are constructed */
+static timings_t base_timings[] = {
+
+  /* FIXME: These are almost certainly wrong in many places, but I don't know
+            what the correct values are. Corrections very welcome */
+
+                   /* /- Horizonal -\  /-- Vertical -\ */
+  { 3500000,       0, 24, 128, 24, 48, 48, 192, 48, 24, 14336 }, /* 48K */
+  { 3528000,       0, 24, 128, 24, 48, 48, 192, 48, 27, 14336 }, /* TC2048 */
+  { 3546900, 1773400, 24, 128, 24, 52, 48, 192, 48, 23, 14361 }, /* 128K */
+  { 3546900, 1773400, 24, 128, 24, 52, 48, 192, 48, 23, 14361 }, /* +2 */
+  { 3546900, 1773400, 36, 128, 28, 32, 64, 192, 48, 16, 17988 }, /* Pentagon */
+  { 3546900, 1773400, 24, 128, 24, 52, 48, 192, 48, 23, 14361 }, /* +2A */
+  { 3546900, 1773400, 24, 128, 24, 52, 48, 192, 48, 23, 14361 }, /* +3 */
+  { 0 },					          /* Unknown machine */
+  { 3500000,       0, 24, 128, 24, 48, 48, 192, 48, 24, 14336 }, /* 16K */
+  { 3528000, 1764750, 24, 128, 24, 48, 48, 192, 48, 27, 14336 }, /* TC2068 */
+};
+
+libspectrum_dword
+libspectrum_timings_processor_speed( libspectrum_machine machine )
+{
+  return base_timings[ machine ].processor_speed;
+}
+
+libspectrum_dword
+libspectrum_timings_ay_speed( libspectrum_machine machine )
+{
+  return base_timings[ machine ].ay_speed;
+}
+
+libspectrum_word
+libspectrum_timings_left_border( libspectrum_machine machine )
+{
+  return base_timings[ machine ].left_border;
+}
+
+libspectrum_word
+libspectrum_timings_horizontal_screen( libspectrum_machine machine )
+{
+  return base_timings[ machine ].horizontal_screen;
+}
+
+libspectrum_word
+libspectrum_timings_right_border( libspectrum_machine machine )
+{
+  return base_timings[ machine ].right_border;
+}
+
+libspectrum_word
+libspectrum_timings_horizontal_retrace( libspectrum_machine machine )
+{
+  return base_timings[ machine ].horizontal_retrace;
+}
+
+libspectrum_word
+libspectrum_timings_top_border( libspectrum_machine machine )
+{
+  return base_timings[ machine ].top_border;
+}
+
+libspectrum_word
+libspectrum_timings_vertical_screen( libspectrum_machine machine )
+{
+  return base_timings[ machine ].vertical_screen;
+}
+
+libspectrum_word
+libspectrum_timings_bottom_border( libspectrum_machine machine )
+{
+  return base_timings[ machine ].bottom_border;
+}
+
+libspectrum_word
+libspectrum_timings_vertical_retrace( libspectrum_machine machine )
+{
+  return base_timings[ machine ].vertical_retrace;
+}
+
+libspectrum_word
+libspectrum_timings_top_left_pixel( libspectrum_machine machine )
+{
+  return base_timings[ machine ].top_left_pixel;
+}
+
+libspectrum_word
+libspectrum_timings_tstates_per_line( libspectrum_machine machine )
+{
+  return base_timings[ machine ].left_border        +
+         base_timings[ machine ].horizontal_screen  +
+         base_timings[ machine ].right_border       +
+	 base_timings[ machine ].horizontal_retrace;
+}
+
+libspectrum_word
+libspectrum_timings_lines_per_frame( libspectrum_machine machine )
+{
+  return base_timings[ machine ].top_border       +
+	 base_timings[ machine ].vertical_screen  +
+	 base_timings[ machine ].bottom_border    +
+	 base_timings[ machine ].vertical_retrace;
+}
+
+libspectrum_dword
+libspectrum_timings_tstates_per_frame( libspectrum_machine machine )
+{
+  return libspectrum_timings_tstates_per_line( machine ) *
+    ( (libspectrum_dword)libspectrum_timings_lines_per_frame( machine ) );
+}
