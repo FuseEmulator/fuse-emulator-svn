018f3502bb04bf0911ac7ef04c754f3c3ec8e2a0
pak21 <pak21@82cbdd72-22ac-47d8-a7df-4a7c34dd7743>
2004-01-29 23:17:34 +0000

    Prevent the 'Secure memory is not locked into core' message appearing.
    git-svn-id: svn://svn.code.sf.net/p/fuse-emulator/code/branches/libspectrum_0_2_1-branch@1943 82cbdd72-22ac-47d8-a7df-4a7c34dd7743

--- libspectrum/libspectrum.c
+++ libspectrum/libspectrum.c
@@ -49,6 +49,9 @@ static const char *gcrypt_version;
 libspectrum_error_function_t libspectrum_error_function =
   libspectrum_default_error_function;
 
+static void
+gcrypt_log_handler( void *opaque, int level, const char *format, va_list ap );
+
 /* Initialise the library */
 libspectrum_error
 libspectrum_init( void )
@@ -67,12 +70,17 @@ libspectrum_init( void )
       return LIBSPECTRUM_ERROR_LOGIC;	/* FIXME: better error code */
     }
 
-    /* Give me some 'secure' memory */
+    /* Ugly hack to prevent the 'Secure memory is not locked into
+       core' message appearing */
+    gcry_set_log_handler( gcrypt_log_handler, NULL );
+
+    /* Initialise the 'secure' memory (which probably won't actually
+       be scure, but that doesn't matter as libspectrum's 'security'
+       is bogus anyway) */
     gcry_control( GCRYCTL_INIT_SECMEM, 16384 );
 
-    /* But it's not very secure as we're not setuid root. However, this
-       isn't critical here, so don't warn about it */
-    gcry_control( GCRYCTL_SUSPEND_SECMEM_WARN );
+    /* Restore the default log handler */
+    gcry_set_log_handler( NULL, NULL );
 
     gcry_control( GCRYCTL_INITIALIZATION_FINISHED );
   }
@@ -84,6 +92,12 @@ libspectrum_init( void )
 #endif				/* #ifdef HAVE_GCRYPT_H */
 
   return LIBSPECTRUM_ERROR_NONE;
+}
+
+static void
+gcrypt_log_handler( void *opaque, int level, const char *format, va_list ap )
+{
+  /* Do nothing */
 }
 
 int
